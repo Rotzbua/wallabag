e2f16fbc7b16cc7bc0957c8632d7fbcca410945a
Rotzbua <Rotzbua@users.noreply.github.com>
2023-09-17 19:48:53 +0200

    Update font `Lato`
    Change source to fontsource repository
    
    Fix nodejs dependency: move from dev to productive

--- app/Resources/static/themes/_global/index.js
+++ app/Resources/static/themes/_global/index.js
@@ -9,10 +9,10 @@ import 'mathjax/es5/tex-svg';
 
 /* Fonts */
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
-import 'lato-font/css/lato-font.css';
 import 'open-dyslexic/open-dyslexic-regular.css';
 import '@fontsource/atkinson-hyperlegible';
 import '@fontsource/eb-garamond';
+import '@fontsource/lato';
 import '@fontsource/montserrat';
 import '@fontsource/oswald';
 import './global.scss';
